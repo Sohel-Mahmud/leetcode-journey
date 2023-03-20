a9ceb5804bdffdb471d473e3aeafc4c834fcbaca
sohel-mahmud <sohel7avon@gmail.com>
2023-03-20 16:30:29 +0600

    daily challange

---
+++ test_copy.cpp
@@ -0,0 +1,21 @@
+/*
+ * File: test copy.cpp
+ * Project: coding
+ * File Created: Monday, 20th March 2023 1:08:18 pm
+ * Author: Sohel Mahmud (sohel@staffasia.org)
+ * -----
+ * Last Modified: Monday, 20th March 2023 1:08:18 pm
+ * Modified By: Sohel Mahmud (sohel@staffasia.org)
+ * -----
+ * Copyright 2021 - 2023 StaffAsia, StaffAsia
+ */
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+// Driver code
+int main()
+{
+    int a = (int)((2 +1) / 2);
+    cout << a;
+}
